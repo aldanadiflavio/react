4983c33d030d425e79a20db93998c31f8d07931f
Aldana Di Flavio <aldanadiflavio@gmail.com>
2024-04-26 21:06:00 -0300

    twitter card sin componente

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+export function App(){
+    return (
+        <article >
+            <header>
+                <img src="public/avatar.jpg" alt="Avatar" />
+                <div>
+                    <strong>Aldana Tamara Di Flavio</strong>
+                    <span>@alhstar</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
